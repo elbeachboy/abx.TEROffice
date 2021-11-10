ce7ce69182f541d82e4ff069b8952ce56b9617b6
Simon Scherer <simon_scherer@bluewin.ch>
2021-11-10 19:59:42 +0100

    Projekte für "Microservicearchitektur" erstellt

--- source/abx.TEROffice.Data/BLL/Deserialisation.cs
+++ source/abx.TEROffice.Data/BLL/Deserialisation.cs
@@ -55,7 +55,5 @@ namespace abx.TEROffice.Data.BLL
             } 
         }
 
-        public string DataFilePath { get; set; }
-
     }
 }
--- test/abx.TEROffice.test/abx.TEROffice.Data.Test/TestDeserialize.cs
+++ test/abx.TEROffice.test/abx.TEROffice.Data.Test/TestDeserialize.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Runtime.ExceptionServices;
 using abx.TEROffice.Data.BLL;
 using abx.TEROffice.Data.Entities;
 using abx.TEROffice.Data.Interfaces;
+using abx.TEROFfice.Library;
 using Xunit;
 
 namespace abx.TEROffice.test
@@ -10,6 +12,7 @@ namespace abx.TEROffice.test
     {
         private Deserialisation _deserialisation;
         private const string _testFilePath = @"E:\Apps\Diplomarbeit\abx.TEROffice\test\abx.TEROffice.test\Testfiles\msg_5216083_2021-10-06-11-40-40.xml";
+        private const string _corruptFIlePath = @"E:\blubbedi\Diplomarbeit\abx.TEROffice\test\abx.TEROffice.test\Testfiles\msg_5216083_2021-10-06-11-40-40.xml";
 
         private Auszuege _kontrollAuszug = new Auszuege
         {
@@ -24,14 +27,21 @@ namespace abx.TEROffice.test
 
         public TestDeserialize()
         {
-            _deserialisation = new Deserialisation(_testFilePath);
+            _deserialisation = new Deserialisation();
         }
         
         [Fact]
-        public void isdeserialzeSuccesful()
+        public void IsDeserialzeSuccesful()
         {
-            var auszug =_deserialisation.CreateAuszugObjekt();
+            var auszug =_deserialisation.CreateAuszugObjekt(_testFilePath);
             Assert.Equal(auszug.Benutzername,_kontrollAuszug.Benutzername);
         }
+
+        [Fact]
+        public void Failed_to_load_StreamReader()
+        {
+            Assert.Throws<TERofficeException>(() => _deserialisation.CreateAuszugObjekt(_corruptFIlePath));
+        }
+
     }
 }
